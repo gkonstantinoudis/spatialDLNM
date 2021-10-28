bbb5182114cc1450da74ee32bfa16c54b901a082
Garyfallos Konstantinoudis <garyfallos.konstantinoudis@ispm.unibe.ch>
2021-10-28 14:34:04 +0100

    Finalize cleaning relative humidity

--- 3_RetrieveRelativeHumidity.R
+++ 3_RetrieveRelativeHumidity.R
@@ -12,8 +12,6 @@
 ################################################################################
 
 
-
-
 library(ncdf4)
 library(plyr)
 library(tidyr)
@@ -29,21 +27,22 @@ library(FNN)
 library(patchwork)
 library(dplyr)
 library(readxl)
+library(data.table)
+
 
 
 setwd("E:/Postdoc Imperial/Projects/Spatial DLNMs/data/")
 
 # read the files
+# The data was downloaded from: https://cds.climate.copernicus.eu/cdsapp#!/dataset/reanalysis-uerra-europe-single-levels?tab=form
+# and the selection was UERRA-HARMONIE, 2010-2019, all months, all days, all times, NetCDF.
+
 relativehumidity <- nc_open("rh_eu.nc")
 extr.rh <- ncvar_get(relativehumidity, varid="r2")
-
-tmp.rstr <- raster("rh_eu.nc")
 
 # extract space and time
 lon <- ncvar_get(relativehumidity,"longitude")
 lat <- ncvar_get(relativehumidity,"latitude")
-
-
 
 hour <- ncvar_get(relativehumidity,"time")
 # hours since 1900-01-01
@@ -57,8 +56,6 @@ dat <- data.frame(start = seq(from = 1, to = length(hour_tr), by = 4),
 un.hour <- unique(hour_tr)
 un.hour <- un.hour[order(un.hour)]
 dat$date <- un.hour
-
-
 
 
 # function to retrieve daily mean
@@ -82,105 +79,65 @@ GetRH <-
     return(DailyMean(start = X[1], stop = X[2], date = X[3]))
     
   }
-) # approximately 10h
-
+) 
 
 
 TMP <- GetRH
-
-
-
 GetRH <- do.call(rbind, GetRH)
 
-GetRH %>% 
-  mutate(ID = group_indices(., lon, lat)) -> GetRH
+# saveRDS(GetRH, file = "GetRHEU07_19")
+# GetRH <- readRDS("GetRHEU07_19")
+rm(relativehumidity, lat, lon, dat, TMP, extr.rh)
+gc()
 
-saveRDS(GetRH, file = "E:/Postdoc Imperial/Projects/Spatial DLNMs/data/GetRHEU07_19")
+# create an ID
+GetRH$ID <- paste0(GetRH$lon, GetRH$lat)
+GetRH$ID <- as.factor(GetRH$ID)
+GetRH$ID <- as.numeric(GetRH$ID)
 
 
-GetRH <- readRDS("E:/Postdoc Imperial/Projects/Spatial DLNMs/data/GetRHEU07_19")
 tmp.latlon <- GetRH[!duplicated(GetRH$ID),]
 
-# tmp.latlon %>% mutate(lon = if_else(lon>180, lon-360, lon)) -> tmp.latlon
-
 # Now I need to project it on the IT grid
-plot(tmp.latlon[,c(1:2)])
-# Now we need the shp in the UK
+mun <- read_sf("E:/Postdoc Imperial/Projects/Spatial DLNMs/data/ProvCM01012020_g_WGS84.shp")
 
-mun <- read_sf("C:/Users/gkonstan/Desktop/exploratory analysis/Effect modifiers/LTLA 2015/Local_Authority_Districts_(December_2015)_Boundaries.shp")
-
-crs.lcc <- "+proj=lcc +lat_1=48 +lat_2=48 +lat_0=48 +lon_0=8 +x_0=3101971.52403412
-+y_0=2994156.36404286 +a=6371229 +b=6371229 +to_meter=11000"
-
-
+# this projection string is retrieved from the documentation of UERRA-HARMONIE
 crs.lcc <- "+proj=lcc +lat_1=48 +lat_2=48 +lat_0=48 +lon_0=8 +x_0=3101971.52403412
 +y_0=2994156.36404286 +a=6371229 +b=6371229 +to_meter=11000 +to +proj=lonlat
 +ellps=WGS84 +datum=WGS84"
 
 
 # make sure shp and temperature file are in the same projection
-
 DT_sf <- st_as_sf(tmp.latlon[, c("lon", "lat")], coords = c("lon", "lat"), crs = crs.lcc)
 DT_sf <- st_transform(DT_sf, crs = st_crs(mun))
 
-mun %>% filter(str_detect(lad15cd, "^E")) %>% 
+mun %>% 
   st_join(DT_sf, .) %>% 
   mutate(ID = tmp.latlon$ID) %>% 
-  filter(!is.na(objectid)) -> tmpjoin
+  filter(!is.na(SIGLA)) -> tmpjoin
+
 
 tmpjoin %>% st_coordinates() %>% as.data.frame() %>% mutate(ID = tmpjoin$ID) -> tmpjoin
 
 # and merge back to the GetRH
 GetRH <- left_join(GetRH, tmpjoin, by = c("ID" = "ID"))
+gc()
 GetRH <- GetRH[!is.na(GetRH$X),]
 gc()
-
 GetRH$lon <- GetRH$lat <- NULL
 
+# saveRDS(GetRH, file = "E:/Postdoc Imperial/Projects/Spatial DLNMs/data/GetRHIT_10_19")
 
-select.date <- "2007-06-01"
-GetRH <- GetRH[,c("ID", "X", "Y", "date", "relativehumidity")]
-GetRH %>% filter(date == select.date) %>% 
-  ggplot() + geom_point(aes(x = X, y = Y, col = relativehumidity), size = 4) + 
-  scale_colour_viridis_c(alpha = 0.6)
+# A quick check
+# select.date <- "2011-06-01"
+# GetRH <- GetRH[,c("ID", "X", "Y", "date", "relativehumidity")]
+# GetRH %>% filter(date == select.date) %>% 
+#   ggplot() + geom_point(aes(x = X, y = Y, col = relativehumidity), size = 4) + 
+#   scale_colour_viridis_c(alpha = 0.6)
   
-# test it with the relative humidity of the file you created
-
-rh_mod <- readRDS("C:/Users/gkonstan/Desktop/exploratory analysis/SSRS/move_2/RH_Predictions2007_2019_10km_lags")
-
-rh_mod %>% filter(days == select.date) %>% 
-  ggplot() + geom_point(aes(x = coords.x1, y = coords.x2, col = RH_median), size = 4) + 
-  scale_colour_viridis_c(alpha = 0.6, limits = c(65, 100)/100)
 
 
-
-
-
-
-GetRH %>% filter(date == select.date) -> tt
-
-
-
-
-
-
-GetRH <- GetRH[,c("ID", "lon", "lat", "date", "relativehumidity")]
-GetRH %>% filter(date == select.date) %>% 
-  ggplot() + geom_point(aes(x = X, y = Y, col = relativehumidity), size = 4) + 
-  scale_colour_viridis_c(limits = c(65, 100))
-
-
-
-GetRH %>% filter(date == select.date) %>% 
-  mutate(lon = if_else(lon>180, lon-360, lon)) %>% 
-  ggplot() + geom_point(aes(x = lon, y = lat, col = relativehumidity), size = 4) + 
-  scale_colour_viridis_c(alpha = 0.6)
-
-
-
-png("tmpplot.png", width = 20, height = 20, units = "cm", res = 300)
-tt %>% mutate(lon = if_else(lon>180, lon-360, lon)) %>% 
-  ggplot() + geom_point(aes(x = lon, y = lat, col = relativehumidity), size = 3) + 
-  scale_colour_viridis_c(alpha = 0.6, limits = c(65, 100))
-dev.off()
-
+########################################################################################################
+########################################################################################################
+########################################################################################################
+########################################################################################################
